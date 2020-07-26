161a874537986393128250c11372e61e5f670f25
take-iwiw <take.iwiw2222@gmail.com>
2020-07-26 16:48:10 +0900

    modify

--- basic/Main.cpp
+++ basic/Main.cpp
@@ -14,51 +14,52 @@
 /*** Macro ***/
 /* Settings */
 typedef struct {
-	int x0;
-	int y0;
-	int x1;
-	int y1;
+	int x;
+	int y;
+	int width;
+	int height;
 } RECT;
 
+typedef struct {
+	cv::Ptr<cv::Tracker> tracker;
+	int numLost;
+	RECT rectFirst;
+} OBJECT_TRACKER;
 
+enum {
+	STATUS_NONE,
+	STATUS_DRAG,
+	STATUS_SELECTED,
+} s_status;
 static RECT s_selectedArea;
-static bool isAreaSelected = false;
 
-void mouseCallback(int event, int x, int y, int flags, void *userdata)
+void mouseCallback(int event, int mouseX, int mouseY, int flags, void *userdata)
 {
+	
 	static bool isDrag = false;
-	static RECT s_mouseDrag;
+	static cv::Point s_dragStart;
 	switch (event) {
 	case cv::EVENT_LBUTTONDOWN:
-		isDrag = true;
-		s_mouseDrag.x0 = x;
-		s_mouseDrag.y0 = y;
+		s_status = STATUS_DRAG;
+		s_dragStart.x = mouseX;
+		s_dragStart.y = mouseY;
 		break;
 	
 	case cv::EVENT_LBUTTONUP:
-		isDrag = false;
-		isAreaSelected = true;
+		s_status = STATUS_SELECTED;
 	case cv::EVENT_MOUSEMOVE:
-		if (isDrag) {
-			s_mouseDrag.x1 = x;
-			s_mouseDrag.y1 = y;
-			s_selectedArea = s_mouseDrag;
-			if (s_selectedArea.x0 > s_selectedArea.x1) {
-				int temp = s_selectedArea.x0;
-				s_selectedArea.x0 = s_selectedArea.x1;
-				s_selectedArea.x1 = temp;
-			}
-			if (s_selectedArea.y0 > s_selectedArea.y1) {
-				int temp = s_selectedArea.y0;
-				s_selectedArea.y0 = s_selectedArea.y1;
-				s_selectedArea.y1 = temp;
-			}
+		if (s_status == STATUS_DRAG) {
+			s_selectedArea.x = std::min(mouseX, s_dragStart.x);
+			s_selectedArea.y = std::min(mouseY, s_dragStart.y);
+			s_selectedArea.width = std::abs(mouseX - s_dragStart.x);
+			s_selectedArea.height = std::abs(mouseY - s_dragStart.y);
 		}
 		break;
 	default:
 		break;
 	}
 }
+
 inline cv::Ptr<cv::Tracker> createTrackerByName(cv::String name)
 {
 	cv::Ptr<cv::Tracker> tracker;
@@ -95,7 +96,7 @@ int main()
 	cap.set(cv::CAP_PROP_FRAME_HEIGHT, originalImageHeight);
 
 	/* Initialize list for tracker */
-	std::list<cv::Ptr<cv::Tracker>> trackerList;
+	std::vector<OBJECT_TRACKER> objectList;
 
 	while (1) {
 		/*** Read image ***/
@@ -104,31 +105,41 @@ int main()
 
 		cv::setMouseCallback("window", mouseCallback);
 		
-		cv::rectangle(mat, cv::Rect(s_selectedArea.x0, s_selectedArea.y0, s_selectedArea.x1 - s_selectedArea.x0, s_selectedArea.y1 - s_selectedArea.y0), cv::Scalar(0, 255, 0), 3);
-		if (isAreaSelected) {
+		cv::rectangle(mat, cv::Rect(s_selectedArea.x, s_selectedArea.y, s_selectedArea.width, s_selectedArea.height), cv::Scalar(0, 255, 0), 3);
+		if (s_status == STATUS_SELECTED) {
 			/* Add a new tracker for the selected area */
-			isAreaSelected = false;
-			auto tracker = createTrackerByName("KCF");
-			tracker->init(mat, cv::Rect(s_selectedArea.x0, s_selectedArea.y0, s_selectedArea.x1 - s_selectedArea.x0, s_selectedArea.y1 - s_selectedArea.y0));
-			trackerList.push_back(tracker);
+			s_status = STATUS_NONE;
+			OBJECT_TRACKER object;
+			object.tracker = createTrackerByName("KCF");
+			object.tracker->init(mat, cv::Rect(s_selectedArea.x, s_selectedArea.y, s_selectedArea.width, s_selectedArea.height));
+			object.numLost = 0;
+			object.rectFirst = s_selectedArea;
+			objectList.push_back(object);
 		}
-
-		for (auto tracker : trackerList) {
+		for (auto it = objectList.begin(); it != objectList.end();) {
+			auto tracker = it->tracker;
 			cv::Rect2d trackedRect;
 			if (tracker->update(mat, trackedRect)) {
 				cv::rectangle(mat, trackedRect, cv::Scalar(255, 0, 0), 2, 1);
+				it->numLost = 0;
+				it++;
 			} else {
-				printf("fail\n");
-				trackerList.remove(tracker);
-				tracker.release();
+				printf("lost\n");
+				if (++(it->numLost) > 100) {
+					printf("delete\n");
+					it = objectList.erase(it);
+					tracker.release();
+				} else {
+					it++;
+				}
 			}
 		}
-			
+		
 		cv::imshow("window", mat);
 		if (cv::waitKey(1) == 'q') break;
 	}
-	for (auto tracker : trackerList) {
-		tracker.release();
+	for (auto obj : objectList) {
+		obj.tracker.release();
 	}
 
 	return 0;
